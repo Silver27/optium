da9b83c6ceb095bb3dc4f55f0f25a6d822946d9c
Silver27 <vincent.miranda97@gmail.com>
2016-12-02 17:43:42 +0800

    Update DB.scala

--- app/models/DB.scala
+++ app/models/DB.scala
@@ -9,8 +9,8 @@ case class Orders(name: String, address: String, pDay: String, item: Seq[Map[Str
 case class Stocks(name: String, quantity: Int, sold: Int, returned: Int)
 
 object DB extends Instance(entities = Seq(Entity[Order](), Entity[Orders](), Entity[Items](), Entity[Stocks]()),
-  url = "jdbc:postgresql://localhost/play",
-  user = "postgres",
-  password = "optium",
+  url = "jdbc:postgresql://	bmqjvn5gqpjrypr-postgresql.services.clever-cloud.com/bmqjvn5gqpjrypr",
+  user = "	u3cktkz8my1gahwseumx",
+  password = "sFXgPfQc9UvnoJ1di3kG",
   initMode = InitMode.Create
 )
